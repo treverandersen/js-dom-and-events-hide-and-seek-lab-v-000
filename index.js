ead3e66c4396afaa5d02756582640aa36884878b
Trever Andersen <trever.r.andersen@gmail.com>
2018-02-08 20:56:17 +0000

    Done.

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ function deepestChild() {
 }
 
 function increaseRankBy(n) {
-  var ul = document.getElementsByClassName('ranked-list');
-  var items = ul.getElementsByTagName('li');
-  for (var i = 0; i < items.length; ++i) {
-    return parseInt(items[i]) + n
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
   }
 }
